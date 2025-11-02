3cad045b555553c1a69fdfd437ec38049133d8cb
RoyRu26 <roy.rutzky@gmail.com>
2025-11-02 16:27:24 +0200

    Complete HW1

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,13 @@
-// Generates three integer random numbers in a given range,
-// and prints them in increasing order.
+
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int ran1 = (int) (Math.random() * lim);
+		int ran2 = (int) (Math.random() * lim);
+		int ran3 = (int) (Math.random() * lim);
+		int max = Math.max(ran1, Math.max(ran2, ran3));
+		int min = Math.min(ran1, Math.min(ran2, ran3));
+		int mid = (ran1 + ran2 + ran3) - (min + max);
+		System.out.println(ran1 + " " + ran2 + " " + ran3 + "\n" + min + " " + mid + " " + max);
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,9 @@
-// Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		int ones = (int) num % 10;
+		int tens = ((int) num % 100) / 10;
+		int hundreds = (int) num / 100;
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones");
 	}
 }
