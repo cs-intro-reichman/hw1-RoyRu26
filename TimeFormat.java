9ea86edecf4742e787003a11f0a766b9c8e5af71
RoyRu26 <roy.rutzky@gmail.com>
2025-11-02 17:37:58 +0200

    roy HW1 complete

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,4 +1,3 @@
-// Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
